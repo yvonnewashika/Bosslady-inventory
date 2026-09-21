fabb85e451caa92cdd06b955ad54df867ac8857e
yvonnewashika <ywashika@gmail.com>
2026-09-21 10:49:42 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/inventory-love/",
+  base: "/Bosslady-inventory/",
 
   server: {
     port: 8080,
