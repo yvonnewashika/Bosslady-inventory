822fe69e736c06762f32db7526b435958ff9929b
yvonnewashika <ywashika@gmail.com>
2026-09-21 10:51:16 +0300

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -8,7 +8,7 @@ export const getRouter = () => {
   const router = createRouter({
     routeTree,
     context: { queryClient },
-    basepath: "/inventory-love",
+    basepath: "/Bosslady-inventory",
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
   });
