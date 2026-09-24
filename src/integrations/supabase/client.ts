64ec98eb8905a08905efa218776cd7e5502cc7fb
yvonnewashika <ywashika@gmail.com>
2026-09-24 12:10:22 +0300

    Fix GitHub Pages deployment

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,13 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 import type { Database } from "./types";
 
-const SUPABASE_URL =
-  import.meta.env.VITE_SUPABASE_URL || process.env.SUPABASE_URL;
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 
 const SUPABASE_PUBLISHABLE_KEY =
-  import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY ||
-  process.env.SUPABASE_PUBLISHABLE_KEY;
-  console.log("SUPABASE URL:", SUPABASE_URL);
+  import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+
+console.log("SUPABASE URL:", SUPABASE_URL);
 
 if (!SUPABASE_URL || !SUPABASE_PUBLISHABLE_KEY) {
   const missing = [
--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,23 @@
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   base: "/Bosslady-inventory/",
-
   server: {
     port: 8080,
-    host: "0.0.0.0",
   },
-
   resolve: {
     tsconfigPaths: true,
   },
-
   plugins: [
-    tailwindcss(),
-<<<<<<< HEAD
     tanstackStart({
       spa: {
         enabled: true,
       },
     }),
-=======
-    tanstackStart(),
->>>>>>> 5fb61bbe4094b295f409c0685ae1bd5908710752
     react(),
+    tailwindcss(),
   ],
 });
